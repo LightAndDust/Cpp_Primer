a5a2c5e464752e4bb6ee62c9754885f4995e4caf
LightAndDust <soyj@foxmail.com>
2023-10-27 11:31:15 +0800

    Update Ex3.22.cpp

--- Ex3.22.cpp
+++ Ex3.22.cpp
@@ -1,3 +1,9 @@
+/*
+ *Revise the loop that printed the first paragraph in text 
+ *to instead change the elements in text that correspond to 
+ *the first paragraph to all uppercase. 
+ *After you’ve updated text, print its contents.
+*/
 #include <iostream>
 #include <vector>
 #include <string>
